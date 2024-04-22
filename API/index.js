74df413e6c06f93474ca7bf8ea16620c95defda4
Rupretch <dapare207@gmail.com>
2024-04-22 16:19:20 -0400

    new projet

--- API/index.js
+++ API/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.use(express.json());
+//app.use(express.json());
 
+/* Normalement il aurai un Headers
+Server: nom
+Age: 123
+Connection: keep-vivant
+*/
 app.get('/tshirt', (req,res) => {
+    //body 
     res.status(200).send({
         tshirt: 'noir',
         size: 'large'
@@ -20,7 +26,7 @@ app.get('/get', (req,res) => {
 
 let messages = [];
 
-app.get('/message', (req,res) =>{
+app.get('/messages', (req,res) =>{
     res.status(200).send({
         message: messages
     })
@@ -40,3 +46,23 @@ app.listen(
     PORT,
     () => console.log(`Hello world on http://localhost:${PORT}`)
 );
+
+
+/*
+Code de status
+2** = bon
+4** = erreur de ta part
+5** = erreur de serveur
+
+Node Express 2010
+https://expressjs.com
+youtube.com/watch?v=SccSCuHhOw0
+65 535 numéros de port possibles.
+
+GET
+POST
+PUT
+PATCH
+DELETE
+
+ */
