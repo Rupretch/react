5a2b79c806e07d6878e403b051b1c2ab77c7f4cf
Rupretch <dapare207@gmail.com>
2024-05-01 13:25:48 -0400

    acceuil done

--- ProjetFinal/src/Components/Accueil.jsx
+++ ProjetFinal/src/Components/Accueil.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Accueil() {
     return(
         <>
@@ -20,8 +22,8 @@ export default function Accueil() {
                         <div className="col-lg-4">
                             <div className="features-icons-item mx-auto mb-5 mb-lg-0 mb-lg-3">
                                 <div className="features-icons-icon d-flex"><i className="fa-solid fa-tablet-screen-button m-auto text-primary"></i></div>
-                                <h3>API Yu-gi-oh</h3>
-                                <p className="lead mb-0">Click ici pour voir des cartes Yu-Gi-Oh</p>
+                                <Link to="/archive2"><h3>API Magic the Gathering</h3></Link>
+                                <p className="lead mb-0"></p>
                             </div>
                         </div>
                         <div className="col-lg-4">
@@ -32,8 +34,8 @@ export default function Accueil() {
                         <div className="col-lg-4">
                             <div className="features-icons-item mx-auto mb-0 mb-lg-3">
                                 <div className="features-icons-icon d-flex"><i className="fa-solid fa-tablet m-auto text-primary"></i></div>
-                                <h3>API Magic The Gathering</h3>
-                                <p className="lead mb-0">Click ici pour voir des cartes Magic!</p>
+                                <Link to="/archive1"><h3>API Yu-gi-oh</h3></Link>
+                                <p className="lead mb-0"></p>
                             </div>
                         </div>
                     </div>
@@ -42,17 +44,17 @@ export default function Accueil() {
             <section className="showcase">
                 <div className="container-fluid p-0">
                     <div className="row g-0">
-                        <div className="col-lg-6 order-lg-2 text-white showcase-img"></div>
+                        <div className="col-lg-6 order-lg-2 text-white showcase-img1"></div>
                         <div className="col-lg-6 order-lg-1 my-auto showcase-text">
                             <h2>Yu-Gi-Oh</h2>
-                            <p className="lead mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore adipisci atque, distinctio omnis aliquid sapiente eius maiores repudiandae doloribus ex et minima numquam, nostrum explicabo vel quasi fuga iusto earum.</p>
+                            <p className="lead mb-0">Yu-Gi-Oh! TCG est un jeu de cartes à collectionner créé en 1999 au Japon. Il est inspiré du manga Yu-Gi-Oh!. Une partie, pouvant être appelée duel, voit s'affronter plusieurs joueurs (souvent un nombre pair) répartis en deux camps, au cours de tours successifs.</p>
                         </div>
                     </div>
                     <div className="row g-0">
-                        <div className="col-lg-6 text-white showcase-img" ></div>
+                        <div className="col-lg-6 text-white showcase-img2"></div>
                         <div className="col-lg-6 my-auto showcase-text">
                             <h2>Magic The Gathering</h2>
-                            <p className="lead mb-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, aspernatur. Iusto odio sunt et. Deleniti culpa omnis nostrum, perferendis cum officiis, atque, quisquam earum aut numquam fugit provident itaque ullam!</p>
+                            <p className="lead mb-0">Magic The Gathering est un jeu de cartes à jouer et à collectionner inventé par Richard Garfield en 1993. Il est souvent cité comme le jeu référence dans le monde des jeux de cartes à jouer.Ce jeu se distingue des jeux de cartes traditionnels par le fait qu'il existe plus de 25 000 cartes différentes avec lesquelles chaque joueur doit construire son propre deck.</p>
                         </div>
                     </div>
                 </div>
